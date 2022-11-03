fcbbcb5d81d8e81d860769d2bac87ca1aaf6afb6
Hermann1710 <Hermann.kobert@gmx.net>
2022-11-03 18:48:54 +0100

    Init START

--- src/test/java/com/example/clientsservice/ClientsServiceApplicationTests.java
+++ src/test/java/com/example/clientsservice/ClientsServiceApplicationTests.java
@@ -1,10 +1,15 @@
 package com.example.clientsservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ClientsServiceApplicationTests {
+    public static void main(String[] args) {
+        SpringApplication.run(ClientsServiceApplicationTests.class);
+        System.out.println("START");
+    }
 
     @Test
     void contextLoads() {
