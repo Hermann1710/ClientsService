893fff2a6d9e04dc0f6a9dbc0d2f915f58c343d8
Hermann1710 <Hermann.kobert@gmx.net>
2022-11-03 18:42:36 +0100

    Init Second

---
+++ src/test/java/com/example/clientsservice/ClientsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clientsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
